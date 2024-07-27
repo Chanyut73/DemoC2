afe157154dba7fa21afeafd38e36d832d0a0dbbd
Chanyut73 <119647765+Chanyut73@users.noreply.github.com>
2024-07-27 20:57:25 +0700

    first commit
    first commit

---
+++ DemoC2/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1722088312,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/playbutton-sheet0.png",
+		"images/ground2-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/fruit-sheet0.png",
+		"images/menu-sheet0.png",
+		"images/replay-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/windowsสำเนา-sheet0.png",
+		"images/windowsสำเนา6-sheet0.png",
+		"images/windowsสำเนา7-sheet0.png",
+		"images/windowsสำเนา9-sheet0.png",
+		"images/windows-sheet0.png",
+		"images/windowsสำเนา10-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
